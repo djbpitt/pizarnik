fea8e3672684203479848d8c03b965066a0cd384
djbpitt <djbpitt@pitt.edu>
2016-03-28 06:31:07 -0400

    First commit

---
+++ pizarnik.py
@@ -0,0 +1,90 @@
+"""
+Collate pyzarnik.xml
+David J. Birnbaum (djbpitt@gmail.com)
+First version: 2016-03-27
+
+Pull parser code lightly adapted from Ronald Haentjens Dekker's
+    https://github.com/rhdekker/python_xml_pull_parser_example/blob/master/pull_parser_test.py
+"""
+import sys
+import re
+from xml.dom.minidom import Element, Text
+from xml.dom.pulldom import CHARACTERS, START_ELEMENT, parseString, END_ELEMENT
+
+# Use djb development version of collatex (https://github.com/djbpitt/collatex, "experimental" branch)
+sys.path.append('/Users/djb/collatex/collatex-pythonport/')
+from collatex import *
+
+class Stack(list):
+    def push(self, item):
+        self.append(item)
+
+    def peek(self):
+        return self[-1]
+
+# Initialize input and output
+source = open('pizarnik.xml','r').read()
+doc = parseString(source)
+witnesses = {}
+
+# Only process content inside witnesses
+inWitness = False
+
+# Tokenize, keeping leading whitespace (whitespace after last token is processed separately)
+def tokenize(contents):
+    return re.findall(r'\s*\S+', contents)
+
+# Regex
+startWhite = re.compile(r'\s+') # strip leading whitespace; match() is automatically anchored at the start
+endWhite = re.compile(r'\S\s+$') # test for trailing whitespace to include in output
+
+for event, node in doc:
+    if event == START_ELEMENT and node.tagName == 'version':
+        # create dictionary entry: key = @wit identifier and value = list of tokens
+        inWitness = True
+        witnesses[node.getAttribute('wit')] = Stack()
+        currentWitness = witnesses[node.getAttribute('wit')]
+        currentWitness.push(Element('reading'))
+    elif not inWitness:
+        # ignore events not inside a witness
+        continue
+    elif event == END_ELEMENT and node.tagName == 'version':
+        inWitness = False
+    # Events below here fire only in inside witness
+    elif event == START_ELEMENT:
+        # <l> elements are not flattened; all others are
+        if node.tagName == 'l':
+            t = Text()
+            t.data = '\n'
+            currentWitness.peek().appendChild(t)
+            currentWitness.peek().appendChild(node)
+            currentWitness.push(node)
+        else:
+            node.setAttribute('type', 'start')
+            currentWitness.peek().appendChild(node)
+    elif event == END_ELEMENT:
+        # Create matching empty "end" element for any element except <l>
+        if node.tagName != 'l':
+            clone = node.cloneNode(False)
+            clone.setAttribute('type', 'end')
+            currentWitness.peek().appendChild(clone)
+        else:
+            currentWitness.pop()
+    elif event == CHARACTERS:
+        # tokens have optional leading whitespace
+        tokens = tokenize(node.data)
+        textdata = ''
+        for token in tokens:
+            if startWhite.match(token):
+                # if there's leading whitespace replace it with a new line
+                textdata += '\n'
+            textdata += startWhite.sub('',token)
+        if endWhite.search(node.data):
+            # if there was whitespace at the end of character data, output a new line
+            textdata += '\n'
+        t = Text()
+        t.data = textdata
+        currentWitness.peek().appendChild(t)
+for key, value in witnesses.items():
+    if key == 'draft':
+        print(value.pop().toxml())
---
+++ sample.py
@@ -0,0 +1,70 @@
+import re
+from xml.dom.minidom import Element, Text
+from xml.dom.pulldom import CHARACTERS, START_ELEMENT, parseString, END_ELEMENT
+
+"""
+ XML pull parser and minidom demo
+ @author: Ronald Haentjens Dekker
+"""
+
+
+class Stack(list):
+    def push(self, item):
+        self.append(item)
+
+    def peek(self):
+        return self[-1]
+
+
+def tokenize(contents):
+    return re.findall("[.?!,;:]+[\\s]*|[^.?!,;:\\s]+[\\s]*", contents)
+
+source = """<rdg wit="#ipa">рускаꙗ землѧ <lb/>
+                                <add place="margin">и хто в неи почалъ пѣрвѣе кнѧжи<hi rend="sup"
+                                        >т</hi></add>·:·</rdg>"""
+# init input
+doc = parseString(source)
+
+# init output
+output = Element("output")
+open_elements = Stack()
+open_elements.push(output)
+
+for event, node in doc:
+    # debug
+    # print(event, node)
+    if event == START_ELEMENT:
+        # skip rdg element
+        if node.localName == "rdg":
+            continue
+        # in case of add deal with overlapping hierarchies
+        if node.localName == "add":
+            # set type attribute to start and add node as a child to output
+            node.setAttribute("type","start")
+            open_elements.peek().appendChild(node)
+        else:
+            open_elements.peek().appendChild(node)
+            open_elements.push(node)
+    elif event == END_ELEMENT:
+        # skip rdg element
+        if node.localName == "rdg":
+            continue
+        # in case of add deal with overlapping hierarchies
+        if node.localName == "add":
+            # create a clone of the node and set type attribute to end and add node as a child to output
+            clone = node.cloneNode(False)
+            clone.setAttribute("type","end")
+            open_elements.peek().appendChild(clone)
+        else:
+            open_elements.pop()
+    elif event == CHARACTERS:
+        tokens = tokenize(node.data)
+        if tokens:
+            textdata = "\n".join(tokens)
+        else:
+            textdata = re.sub("[^\S\n]+", "", node.data)
+        t = Text()
+        t.data = textdata
+        open_elements.peek().appendChild(t)
+
+print(output.toxml())
